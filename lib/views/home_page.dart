21fcba88142786e4870b27d02e1c80cb968b66d2
Waaarisra <warisara.tan@ku.th>
2023-02-27 01:18:13 +0700

    start profile

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,8 +3,11 @@ import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   List testData = [
-    {'name': 'Program 1'},
-    {'name': 'Program 2'}
+    {'name': 'Program 1', 'Type': 'Abs'},
+    {'name': 'Program 2', 'Type': 'Workout'},
+    {'name': 'Program 3', 'Type': 'Workout'},
+    {'name': 'Program 4', 'Type': 'Workout'},
+    {'name': 'Program 5', 'Type': 'Workout'}
   ];
 
   @override
@@ -13,54 +16,154 @@ class HomePage extends StatelessWidget {
         body: SafeArea(
       child: Padding(
         padding: const EdgeInsets.only(left: 20),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'WORKOUT PLANNER',
-                style: GoogleFonts.prompt(
-                  fontSize: 32,
-                  fontStyle: FontStyle.italic,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'WORKOUT PLANNER',
+                  style: GoogleFonts.prompt(
+                    fontSize: 32,
+                    fontStyle: FontStyle.italic,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 30),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text('Today\'s Programs',
-                  style: GoogleFonts.prompt(
-                    textStyle: Theme.of(context).textTheme.bodyText1,
-                  )),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  buildCard(),
-                  SizedBox(width: 20),
-                  buildCard(),
-                  SizedBox(width: 20),
-                  buildCard()
-                ],
+              SizedBox(height: 30),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('Today\'s Programs',
+                    style: GoogleFonts.prompt(
+                      textStyle: Theme.of(context).textTheme.bodyText1,
+                    )),
               ),
-            )
-          ],
-          // ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 100,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: testData.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 200,
+                      child: Card(
+                        child: ListTile(
+                          title: Text(testData[index]['name']),
+                          subtitle: Text(testData[index]['Type']),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('Weight Training Programs',
+                    style: GoogleFonts.prompt(
+                      textStyle: Theme.of(context).textTheme.bodyText1,
+                    )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 150,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: testData.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 300,
+                      child: Card(
+                        child: ListTile(
+                          title: Text(testData[index]['name']),
+                          subtitle: Text(testData[index]['Type']),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('Body Weight Programs',
+                    style: GoogleFonts.prompt(
+                      textStyle: Theme.of(context).textTheme.bodyText1,
+                    )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 150,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: testData.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 300,
+                      child: Card(
+                        child: ListTile(
+                          title: Text(testData[index]['name']),
+                          subtitle: Text(testData[index]['Type']),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('Cardio Programs',
+                    style: GoogleFonts.prompt(
+                      textStyle: Theme.of(context).textTheme.bodyText1,
+                    )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 150,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: testData.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 300,
+                      child: Card(
+                        child: ListTile(
+                          title: Text(testData[index]['name']),
+                          subtitle: Text(testData[index]['Type']),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+            // ),
+          ),
         ),
       ),
     ));
   }
 
   Widget buildCard() => Container(
-        width: 150,
-        height: 40,
-        color: Colors.red,
+        padding: EdgeInsets.all(100),
+        decoration: BoxDecoration(
+            color: Colors.red, borderRadius: BorderRadius.circular(12)),
       );
 }
