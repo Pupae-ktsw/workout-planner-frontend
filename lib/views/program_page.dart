9952a574af86541030b6596a86c69183cbaf6801
Waaarisra <warisara.tan@ku.th>
2023-03-02 18:25:31 +0700

    updated button and dropdown

--- lib/views/program_page.dart
+++ lib/views/program_page.dart
@@ -1,13 +1,103 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProgramPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Program",
+      home: showProgramPage(),
+    );
+  }
+}
+
+class showProgramPage extends StatefulWidget {
+  @override
+  _ProgramPageState createState() => _ProgramPageState();
+}
+
+class _ProgramPageState extends State<showProgramPage> {
+  final _programList = ["All", "On going", "Finished"];
+  String _selectedValue = "All";
+
+  // void _MyFormState() {
+  //   _selectedValue = _programList[0];
+  // }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Program Page'),
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: SafeArea(
+            child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Programs',
+                    style: GoogleFonts.prompt(
+                      fontSize: 32,
+                    ),
+                  ),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.red),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.red),
+                        )),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Add Program',
+                        style: TextStyle(color: Colors.white),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: SizedBox(
+                  width: 100,
+                  child: DropdownButtonFormField(
+                    value: _selectedValue,
+                    items: _programList
+                        .map((e) => DropdownMenuItem(
+                              child: Text(e),
+                              value: e,
+                            ))
+                        .toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        _selectedValue = val as String;
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        )),
       ),
-      body: Center(child: Text('Programs', style: TextStyle(fontSize: 20))),
     );
   }
 }
