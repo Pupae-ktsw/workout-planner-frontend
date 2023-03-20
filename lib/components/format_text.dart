ccbedb338e86f0868c97fe753cdde6b60b0cbf69
Waaarisra <warisara.tan@ku.th>
2023-03-20 18:35:56 +0700

    merge with feature/profile

--- lib/components/format_text.dart
+++ lib/components/format_text.dart
@@ -7,18 +7,9 @@ ThemeData appTheme() {
     textTheme: const TextTheme(
       // login and register screen
       headline1: TextStyle(
-<<<<<<< HEAD:lib/components/formart_text.dart
-          fontSize: 62.0, fontWeight: FontWeight.w700, color: Colors.black),
-
-      headline2: TextStyle(fontSize: 16, color: Colors.black),
-
-      headline3: TextStyle(fontSize: 20, color: Colors.black),
-
-=======
           fontSize: 40.0, fontWeight: FontWeight.w700, color: Colors.black),
       // app bar
       // error msg
->>>>>>> feature/profile:lib/components/format_text.dart
       bodyText1: TextStyle(fontSize: 18.0),
 
       bodyText2: TextStyle(fontSize: 16.0),
--- lib/model/profile.dart
+++
@@ -1,7 +0,0 @@
-class Profile {
-  String email;
-  String password;
-  String name;
-
-  Profile(this.email, this.name, this.password);
-}
