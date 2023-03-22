69a8f310b05f4bb4d5df061324aa2919dc0a45a6
Waaarisra <warisara.tan@ku.th>
2023-03-22 19:16:40 +0700

    no interest

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:frontend/views/home_page.dart';
 import 'package:frontend/views/planner_page.dart';
 import 'package:frontend/views/profile_page.dart';
 import 'package:frontend/views/program_page.dart';
+import 'package:frontend/views/search_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       debugShowCheckedModeBanner: false,
-      home: const BottomNav(),
+      home: const SearchPage(),
     );
   }
 }
