846637fefe5070b67e210fbc3e339e7657567f63
Waaarisra <warisara.tan@ku.th>
2023-03-30 16:10:08 +0700

    edit model dayOfProgram

--- lib/models/dayOfProgram.dart
+++ lib/models/dayOfProgram.dart
@@ -1,3 +1,4 @@
+import 'package:frontend/models/program.dart';
 import 'package:frontend/models/youtubeVid.dart';
 
 class DayOfProgram {
@@ -7,6 +8,7 @@ class DayOfProgram {
   String? dateCalendar;
   String? workoutStatus;
   YoutubeVid? youtubeVid;
+  Program? program;
 
   DayOfProgram({
     this.id,
@@ -15,28 +17,30 @@ class DayOfProgram {
     this.dateCalendar,
     this.workoutStatus,
     this.youtubeVid,
+    this.program,
   });
 
   DayOfProgram.fromJson(Map<String, dynamic> json) {
     id = json['_id'];
-    programId = json['program_id'];
     numberOfDay = json['numberOfDay'];
     dateCalendar = json['dateCalendar'];
     workoutStatus = json['workoutStatus'];
     youtubeVid = json['youtubeVid'] != null
-        ? new YoutubeVid.fromJson(json['youtubeVid'])
+        ? YoutubeVid.fromJson(json['youtubeVid'])
+        : null;
+    program = json['program_id'] != null
+        ? Program.fromJson(json['program_id'])
         : null;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['_id'] = this.id;
-    data['program_id'] = this.programId;
-    data['numberOfDay'] = this.numberOfDay;
-    data['dateCalendar'] = this.dateCalendar;
-    data['workoutStatus'] = this.workoutStatus;
-    if (this.youtubeVid != null) {
-      data['youtubeVid'] = this.youtubeVid!.toJson();
+    data['_id'] = id;
+    data['numberOfDay'] = numberOfDay;
+    data['dateCalendar'] = dateCalendar;
+    data['workoutStatus'] = workoutStatus;
+    if (youtubeVid != null) {
+      data['youtubeVid'] = youtubeVid!.toJson();
     }
 
     return data;
