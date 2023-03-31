4ad567746f63f329b60913b6f978a0b64415d5ca
Waaarisra <warisara.tan@ku.th>
2023-03-31 23:09:40 +0700

    checkout

--- lib/views/dayOfProgram_page.dart
+++ lib/views/dayOfProgram_page.dart
@@ -1,3 +1,4 @@
+import 'package:bordered_text/bordered_text.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/controllers/day_of_program_controller.dart';
 import 'package:frontend/controllers/program_controller.dart';
@@ -41,7 +42,7 @@ class DayOfProgramPage extends StatelessWidget {
                     size: 32,
                   )),
               Text('${program.programName}',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               Container()
             ],
           ),
@@ -62,64 +63,112 @@ class DayOfProgramPage extends StatelessWidget {
                             itemBuilder: (context, index) {
                               var dayOfProgram =
                                   snapshot.data![index] as DayOfProgram;
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    'Day' + ' ' + '${dayOfProgram.numberOfDay}',
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Row(
-                                    children: [
-                                      Container(
-                                        height: 130,
-                                        width: 160,
-                                        child: Image.network(
-                                          dayOfProgram.youtubeVid!.thumbnail ??
-                                              "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%",
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 20.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      'Day' +
+                                          ' ' +
+                                          '${dayOfProgram.numberOfDay}',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Expanded(
+                                          child: Stack(
+                                            children: [
+                                              ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(10),
+                                                child: Image.network(
+                                                  dayOfProgram.youtubeVid!
+                                                          .thumbnail ??
+                                                      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%",
+                                                ),
+                                              ),
+                                              Container(
+                                                decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(10),
+                                                  color: dayOfProgram
+                                                              .workoutStatus ==
+                                                          "Done"
+                                                      ? Colors.black
+                                                          .withOpacity(0.47)
+                                                      : Colors.black
+                                                          .withOpacity(0),
+                                                ),
+                                                child: SizedBox(
+                                                  height: 100,
+                                                  width: 200,
+                                                ),
+                                              ),
+                                              Positioned(
+                                                top: 32,
+                                                left: 62,
+                                                child: BorderedText(
+                                                  strokeWidth: 3,
+                                                  strokeColor: Colors.black,
+                                                  child: dayOfProgram
+                                                              .workoutStatus ==
+                                                          "Done"
+                                                      ? Text(
+                                                          dayOfProgram
+                                                              .workoutStatus!,
+                                                          style: TextStyle(
+                                                              fontSize: 20,
+                                                              color:
+                                                                  Colors.white),
+                                                        )
+                                                      : Text(''),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
                                         ),
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Expanded(
-                                        child: Column(children: [
-                                          SizedBox(
-                                            height: 20,
-                                          ),
-                                          Align(
-                                            alignment: Alignment.topLeft,
-                                            child: Text(
-                                              dayOfProgram.youtubeVid!.title ??
-                                                  "No program",
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.bold),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Expanded(
+                                          child: Column(children: [
+                                            Align(
+                                              alignment: Alignment.topLeft,
+                                              child: Text(
+                                                dayOfProgram
+                                                        .youtubeVid!.title ??
+                                                    "No program",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 16),
+                                              ),
                                             ),
-                                          ),
-                                          SizedBox(
-                                            height: 10,
-                                          ),
-                                          Align(
-                                            alignment: Alignment.topLeft,
-                                            child: Text(
-                                              dayOfProgram
-                                                      .youtubeVid!.channel ??
-                                                  "No program",
+                                            SizedBox(
+                                              height: 5,
                                             ),
-                                          ),
-                                        ]),
-                                      )
-                                    ],
-                                  )
-                                ],
+                                            Align(
+                                              alignment: Alignment.topLeft,
+                                              child: Text(
+                                                dayOfProgram
+                                                        .youtubeVid!.channel ??
+                                                    "No program",
+                                              ),
+                                            ),
+                                          ]),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
                               );
                             },
                           ),
--- lib/views/program_page.dart
+++ lib/views/program_page.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'dart:io';
 import 'dart:math';
 
+import 'package:bordered_text/bordered_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:frontend/controllers/program_controller.dart';
@@ -11,6 +12,7 @@ import 'package:frontend/repositories/program_repo.dart';
 import 'package:frontend/views/addProgram_page.dart';
 import 'package:frontend/views/dayOfProgram_page.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:gradients/gradients.dart';
 import 'package:intl/intl.dart';
 
 class ProgramPage extends StatelessWidget {
@@ -32,6 +34,9 @@ class showProgramPage extends StatefulWidget {
 class _ProgramPageState extends State<showProgramPage> {
   final _programTypeList = ["All", "Challenging", "Completed"];
   String _selectedValue = "All";
+  Color color1 = Colors.black.withOpacity(0.85);
+  Color color2 = Colors.black.withOpacity(0);
+
   var _programController = ProgramController(ProgramRepo());
 
   @override
@@ -77,7 +82,7 @@ class _ProgramPageState extends State<showProgramPage> {
                     ],
                   ),
                   SizedBox(
-                    height: 20,
+                    height: 10,
                   ),
                   Align(
                     alignment: Alignment.topLeft,
@@ -145,60 +150,85 @@ class _ProgramPageState extends State<showProgramPage> {
                                 String? formattedStartDate =
                                     DateFormat('dd/MM/yyyy').format(startDate!);
 
-                                return InkWell(
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                DayOfProgramPage(
-                                                    program: program)));
-                                  },
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                        child: ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          child: Image.network(program
-                                                  .thumbnail ??
-                                              "https://www.shape.com/thmb/DjCIHGX6cWaIniuqHeBAAreNE08=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/best-cardio-exercises-promo-2000-498cbfb8f07541b78572bf810e7fb600.jpg"),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Expanded(
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          children: [
-                                            Align(
-                                              alignment: Alignment.topLeft,
-                                              child: Text(
-                                                program.programName ??
-                                                    "No program",
-                                                style: TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.bold),
+                                return Padding(
+                                  padding: EdgeInsets.only(bottom: 40),
+                                  child: InkWell(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    DayOfProgramPage(
+                                                        program: program)));
+                                      },
+                                      child: Stack(
+                                        children: [
+                                          // Image widget
+                                          ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            child: Image.network(
+                                              program.thumbnail ??
+                                                  "https://www.shape.com/thmb/DjCIHGX6cWaIniuqHeBAAreNE08=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/best-cardio-exercises-promo-2000-498cbfb8f07541b78572bf810e7fb600.jpg",
+                                              width: 400,
+                                              height: 200,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+
+                                          // Colored box overlay
+                                          Container(
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              gradient: LinearGradient(
+                                                begin: Alignment.bottomCenter,
+                                                end: Alignment.topCenter,
+                                                colors: <Color>[color1, color2],
                                               ),
                                             ),
-                                            SizedBox(
-                                              height: 10,
+                                            width: 400,
+                                            height: 200,
+                                          ),
+
+                                          // Text widget
+                                          Positioned(
+                                            bottom: 10,
+                                            left: 10,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                BorderedText(
+                                                  strokeColor: Colors.black,
+                                                  strokeWidth: 2,
+                                                  child: Text(
+                                                    program.programName ??
+                                                        "No program",
+                                                    style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      color: Colors.white,
+                                                      fontSize: 20,
+                                                    ),
+                                                  ),
+                                                ),
+                                                SizedBox(height: 5),
+                                                Text(
+                                                  formattedStartDate == null
+                                                      ? "No date"
+                                                      : "start date : $formattedStartDate",
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white,
+                                                    fontSize: 16,
+                                                  ),
+                                                ),
+                                              ],
                                             ),
-                                            Align(
-                                              alignment: Alignment.topLeft,
-                                              child: Text(
-                                                formattedStartDate == null
-                                                    ? "No date"
-                                                    : "start date : $formattedStartDate",
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
+                                          ),
+                                        ],
+                                      )),
                                 );
                               }))
                           : Center(
